310d6f9e12a023949b111db0f43373d8880a9d5e
Terry Wen <wen.terry.e@gmail.com>
2016-04-02 15:04:55 -0700

    messages

--- LiveMate/InboxCell.swift
+++ LiveMate/InboxCell.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class InboxCell: UITableViewCell {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- LiveMate/InboxViewController.swift
+++ LiveMate/InboxViewController.swift
@@ -7,16 +7,72 @@
 //
 
 import UIKit
+import Parse
 
 class InboxViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var messages: [PFObject] = []
+    var users: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = self
         tableView.delegate = self
+        
+        let query1 = PFQuery(className:"Message")
+        query1.whereKey("fromId", equalTo: PFUser.currentUser()!.objectId!)
+        query1.findObjectsInBackgroundWithBlock {
+            (objects: [PFObject]?, error: NSError?) -> Void in
+            
+            if error == nil {
+                // The find succeeded.
+                print("Successfully retrieved \(objects!.count) messages from user.")
+                // Do something with the found objects
+                for message: PFObject in objects! {
+                    let username = message.objectForKey("to")!.objectForKey("username") as! String
+                    print("\(username)")
+                    if !self.users.contains(username) {
+                        self.users.append(username)
+                    }
+                }
+            } else {
+                // Log details of the failure
+                print("Error: \(error!) \(error!.userInfo)")
+            }
+        }
+        
+        let query2 = PFQuery(className:"Message")
+        query2.whereKey("toId", equalTo: PFUser.currentUser()!.objectId!)
+        query2.findObjectsInBackgroundWithBlock {
+            (objects: [PFObject]?, error: NSError?) -> Void in
+            
+            if error == nil {
+                // The find succeeded.
+                print("Successfully retrieved \(objects!.count) messages to user.")
+                // Do something with the found objects
+                for message: PFObject in objects! {
+                    let username = message.objectForKey("from")!.objectForKey("username") as! String
+                    print("dog\(username)")
+                    if !self.users.contains(username) {
+                        self.users.append(username)
+                    }
+                }
+            } else {
+                // Log details of the failure
+                print("Error: \(error!) \(error!.userInfo)")
+            }
+        }
+        
+        for message in messages {
+            let username = message.objectForKey("from")!.objectForKey("username") as! String
+            print("dog\(username)")
+            if !users.contains(username) {
+                users.append(username)
+            }
+        }
         
         // Do any additional setup after loading the view.
     }
@@ -27,11 +83,14 @@ class InboxViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return users.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCellWithIdentifier("inboxCell", forIndexPath: indexPath) as! InboxCell
+        
+        cell.nameLabel.text = users[indexPath.row]
+        return cell
     }
 
     /*
